72028576ecf78fc999d09637cd585f97d8d04a59
Kedihealthcare <kedihealthcaresolution1@gmail.com>
2026-04-08 10:09:37 +0100

    Clear FIREBASE_API_KEY for security reasons

--- env-config.js
+++ env-config.js
@@ -4,7 +4,7 @@
 window.KEDI_ENV = {
     // GEMINI_API_KEY: "PROXIED", // Moved to backend for security
     OPENAI_API_KEY: "your_openai_key_here",
-    FIREBASE_API_KEY: "AIzaSyB22U5NM_FqOC8P5tMgDPIwz_IzNeYODeA",
+    FIREBASE_API_KEY: "",
     PI_API_KEY: "your_pi_api_key"
 };
     
